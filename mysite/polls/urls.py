600661eadf906ab74df80c510511603ad68f6029
Stanley643 <nmurigi@kabarak.ac.ke>
2023-03-04 16:20:51 +0300

    views update

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:questrion_id>/results/', views.results, name='results'),
+    path('<int:querstion_id>/vote/', views.vote, name='vote')
 ]
