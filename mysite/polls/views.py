6a127061f7e37be0b83ccc5dce6c5ad60d5cf378
Stanley643 <nmurigi@kabarak.ac.ke>
2023-03-09 16:58:10 +0300

    django forms

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
@@ -15,9 +17,28 @@ def index(request):
 def detail(request, question_id):
     return HttpResponse("you're looking at question %s." % question_id)
 
-#def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return  HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question': question})
 
-#def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question':question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def detail(request, question_id):
+   # try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+     #   raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
